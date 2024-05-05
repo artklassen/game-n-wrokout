644f8279223340c73306c904a119d5a328d9a6c4
artklassen <aklassen@smartasset.com>
2024-05-05 17:26:18 -0400

    Fixed Ts errors

--- components/results.tsx
+++ components/results.tsx
@@ -14,6 +14,14 @@ interface Game {
     date?: Date;
 }
 
+interface AggregatedGame extends Game {
+    exercises: { exerciseName: string; exerciseAmount: number }[];
+}
+
+interface AggregatedGames {
+    [key: string]: AggregatedGame;
+}
+
 const Results: React.FC = () => {
     const [games, setGames] = useState<Game[]>([]);
 
@@ -24,23 +32,33 @@ const Results: React.FC = () => {
     };
 
     const aggregateGames = (incomingGames: Game[]) => {
-        const aggregatedGames = incomingGames.reduce((acc, game) => {
+        const aggregatedGames = incomingGames.reduce<AggregatedGames>((acc, game) => {
             const normalizedDate = normalizeDate(game.date);
             const key = `${game.user}-${normalizedDate}`;
-            if (acc[key]) {
-                const existingExercise = acc[key].exercises.find(e => e.exerciseName === game.exerciseName);
-                if (existingExercise) {
-                    existingExercise.exerciseAmount += game.exerciseAmount;
-                } else {
-                    acc[key].exercises.push({ exerciseName: game.exerciseName, exerciseAmount: game.exerciseAmount });
-                }
-            } else {
+
+            if (!acc[key]) {
                 acc[key] = {
                     ...game,
-                    exercises: [{ exerciseName: game.exerciseName, exerciseAmount: game.exerciseAmount }],
-                    date: normalizedDate ? new Date(normalizedDate) : undefined
+                    date: normalizedDate ? new Date(normalizedDate) : undefined,
+                    exercises: [...game.exercises]
                 };
             }
+
+            // Loop over each exercise in the incoming game
+            game.exercises.forEach(incomingExercise => {
+                const existingExercise = acc[key].exercises.find(e => e.exerciseName === incomingExercise.exerciseName);
+                if (existingExercise) {
+                    // Add the exercise amount if the exercise already exists
+                    existingExercise.exerciseAmount += incomingExercise.exerciseAmount;
+                } else {
+                    // Otherwise, add the new exercise to the list
+                    acc[key].exercises.push({
+                        exerciseName: incomingExercise.exerciseName,
+                        exerciseAmount: incomingExercise.exerciseAmount
+                    });
+                }
+            });
+
             return acc;
         }, {});
 
@@ -50,9 +68,14 @@ const Results: React.FC = () => {
     useEffect(() => {
         const eventSource = new EventSource('http://localhost:3002/events');
         eventSource.onmessage = (event) => {
-            const newGames: Game[] = JSON.parse(event.data).map(game => ({
+            const incomingGames = JSON.parse(event.data);
+            const newGames = incomingGames.map((game: { exerciseName: any; exerciseAmount: any; }) => ({
                 ...game,
-                exercises: [{ exerciseName: game.exerciseName, exerciseAmount: game.exerciseAmount }]
+                // Construct the exercises array using the exerciseName and exerciseAmount, if they exist
+                exercises: game.exerciseName && game.exerciseAmount ? [{
+                    exerciseName: game.exerciseName,
+                    exerciseAmount: game.exerciseAmount
+                }] : []
             }));
             const aggregatedGames = aggregateGames(newGames);
             setGames(aggregatedGames);
@@ -61,19 +84,25 @@ const Results: React.FC = () => {
         return () => eventSource.close();
     }, []);
 
+
     return (
         <div className='results-container'>
-            {games.map((g, index) => (
-                <div className='game-card' key={index}>
-                    <p className='date'>Date: {g.date ? g.date.toISOString().split('T')[0] : 'No date provided'}</p>
-                    <p>User: {g.user}</p>
-                    <p>Game: {g.game}</p>
-                    {g.exercises.length > 1 && <p>Exercises:</p>}  {/* Conditional rendering based on the number of exercises */}
-                    {g.exercises.map((ex, exIndex) => (
-                        <p key={exIndex}>{g.exercises.length > 1 ? `${ex.exerciseName} - ${ex.exerciseAmount}` : `Exercise: ${ex.exerciseName} - ${ex.exerciseAmount}`}</p>
-                    ))}
-                </div>
-            ))}
+            {games.map((g, index) => {
+                console.log('Rendering game:', g); // Log each game being rendered
+                return (
+                    <div className='game-card' key={index}>
+                        <p className='date'>Date: {g.date ? g.date.toISOString().split('T')[0] : 'No date provided'}</p>
+                        <p>User: {g.user}</p>
+                        <p>Game: {g.game}</p>
+                        <p>Exercise count: {g.exercises.length}</p>
+                        {g.exercises.length > 0 && <p>Exercises:</p>} {/* Updated condition */}
+                        {g.exercises.map((ex, exIndex) => (
+                            <p key={exIndex}>{ex.exerciseName} - {ex.exerciseAmount}</p>
+                        ))}
+                    </div>
+                );
+            })}
+
         </div>
 
     );
