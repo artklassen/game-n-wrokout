2df8b0f0fa1099cc84516d46aa5e5e6a525f7b65
artklassen <aklassen@smartasset.com>
2024-05-11 12:21:26 -0400

    removed unused files

--- components/results.tsx
+++ components/results.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
 import '@/styles/tailwindcss/components/results.scss';
-import getDataFromPostgress from "@/utils/getDataFromPostgress";
 
 interface Exercise {
     exerciseName: string;
